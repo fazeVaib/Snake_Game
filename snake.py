58176ecf04dd313d83aea90d8ca0ae4a44f6fcac
fazevaib <vaibhavvats123@gmail.com>
2018-03-10 16:53:52 +0530

    Added score and fixed crashes

--- snake.py
+++ snake.py
@@ -9,6 +9,7 @@ w.border(0)
 curses.noecho()
 w.keypad(1)
 w.timeout(100)
+score = 0
 
 snake_x = screenW // 5
 snake_y = screenH // 5
@@ -20,17 +21,21 @@ snake = [
 ]
 
 food = [int(screenH / 2), int(screenW / 2)]
-w.addch(food[0], food[1], '*')
+w.addch(food[0], food[1], '@')
 w.addstr(0, 3, 'Score: ')
 w.addstr(24, 28, 'Snake')
 key = curses.KEY_RIGHT
 
 while True:
     nextKey = w.getch()
+    w.addstr(0, 3, 'Score: ' + str(score))
     key = key if nextKey == -1 else nextKey
 
-    if snake[0][0] in [0, screenH] or snake[0][1] in [0, screenW] or snake[0] in snake[1:]:
+    if snake[0][0] in [0, screenH-1] or \
+            snake[0][1] in [0, screenW-1] or \
+            snake[0] in snake[1:]:
         curses.endwin()
+        print("You scored " + str(score) + ". Better luck next time!")
         quit()
 
     newHead = [snake[0][0], snake[0][1]]
@@ -47,6 +52,7 @@ while True:
     snake.insert(0, newHead)
 
     if snake[0] == food:
+        score += 1
         food = None
         while food is None:
             nf = [
@@ -54,9 +60,9 @@ while True:
                 random.randint(1, screenW - 2)
             ]
             food = nf if nf not in snake else None
-        w.addch(food[0], food[1], '*')
+        w.addch(food[0], food[1], '@')
     else:
         tail = snake.pop()
         w.addch(tail[0], tail[1], ' ')
 
-    w.addch(snake[0][0], snake[0][1], '=')
+    w.addch(snake[0][0], snake[0][1], '*')
